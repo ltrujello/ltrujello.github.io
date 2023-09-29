28e687ce33e151d5ffde52b373597d073b08ca55
ltrujello <ltrujillo@g.hmc.edu>
2023-09-29 15:28:50 -0700

    Deployed 7bfa752 with MkDocs version: 1.5.2

---
+++ algebra/mathjax_helper.js
@@ -0,0 +1,62 @@
+window.MathJax = {
+  tex: {
+    macros: {
+      normal : ["\\unlhd"],
+      im : ["\\mbox{Im}"],
+      nat : ["\\mbox{Nat}"],
+      ZZ : ["\\mathbb{Z}"],
+      RR : ["\\mathbb{R}"],
+      NN : ["\\mathbb{N}"],
+      zz : ["\\mathbb{Z}"],
+      rr : ["\\mathbb{R}"],
+      nn : ["\\mathbb{N}"],
+      qq : ["\\mathbb{Q}"],
+      dom : ["\\mbox{Dom}"],
+      cod : ["\\mbox{Cod}"],
+      id : ["\\mbox{id}"],
+      ob : ["\\mbox{Ob}"],
+      CC : ["\\mathcal{C}"],
+      DD : ["\\mathcal{D}"],
+      BB : ["\\mathcal{B}"],
+      cc : ["\\mathcal{C}"],
+      dd : ["\\mathcal{D}"],
+      bb : ["\\mathcal{B}"],
+      pp : ["\\mathcal{P}}"],
+      ee : ["\\mathcal{E}"],
+      qqq : ["\\mathcal{Q}"],
+      ppp : ["\\mathbb{Q}"],
+      vv : ["\\mathcal{V}"],
+      ff : ["\\mathcal{F}"],
+      gal : ["\\text{Gal}"],
+      aut : ["\\text{Aut}"],
+      cone : ["\\text{Cone}"],
+      fun : ["\\text{Fun}"],
+      op : ["^\{\text{op}}"],
+      ann : ["\\mbox{Ann}"],
+      Lim : ["\\text{Lim }"],
+      Colim : ["\\text{Colim }"],
+      proj : ["\\text{proj}"],
+      join : ["\\vee"],
+      meet : ["\\wedge"],
+      coker : ["\\text{Coker}"],
+      isomarrow : ["\\xrightarrow{\\sim}"],
+      aend : ["\\text{End}}"],
+      spec : ["\\text{Spec}"],
+      path : ["\\text{Path}"],
+      ww : ["\\mathcal{W}"],
+      mm : ["\\mathcal{M}"],
+      ll : ["\\mathcal{L}"],
+      ee : ["\\mathcal{E}"],
+      ss : ["\\mathcal{S}"],
+      wp : ["\\mathcal{W}_{\mathrm{P}}"],
+      phi : ["\\varphi"], 
+      bm: ["\\boldsymbol{#1}",1]
+    }
+  }
+};
+
+
+// MathJax.config.tex['macros'] = {
+//         cc:["\\mathcal{C}"]
+// }
+
